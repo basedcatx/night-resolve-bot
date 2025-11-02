35895166f48dd57aad0ea4babfb7b22f6de9e13b
Tazuh Faith <tazuhf@gmail.com>
2025-11-02 16:13:44 +0100

    Sorry i am committing this to my main branch: Added various tests, including guild and channel creation tests Added a logger function for better console logging and fast

--- tests/classes/managers/guild.test.ts
+++ tests/classes/managers/guild.test.ts
@@ -1,11 +1,15 @@
-import {test, expect} from "bun:test"
-import { botClient } from "../../.."
-import { GuildManager } from "../../../classes/managers/GuildManager";
-import botConfigs from "../../../config";
+import { test, expect } from 'bun:test';
+import { botClient } from '../../..';
+import { GuildManager } from '../../../classes/managers/GuildManager';
+import botConfigs from '../../../config';
 
-test("Verifies that a new guild object is created and could be used to retrieve an instanciated guild name and id", function check() {
+await botClient.login(botConfigs.env.bot.token);
+
+botClient.on('clientReady', async function exec() {
+  test('Verifies that a new guild object is created and could be used to retrieve an instanciated guild name and id', async function () {
     const guild = GuildManager.fromId(botConfigs.env.supportGuild.guildId, botClient);
-    expect (guild).toBeInstanceOf(GuildManager)  
-    expect(typeof guild?.name).toBe("string")
-    expect (typeof guild?.id).toBe("string")
-})
+    expect(guild).toBeInstanceOf(GuildManager);
+    expect(typeof guild?.name).toBe('string');
+    expect(typeof guild?.id).toBe('string');
+  });
+});
